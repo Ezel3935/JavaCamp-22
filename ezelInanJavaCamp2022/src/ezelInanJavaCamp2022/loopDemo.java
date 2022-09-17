9ffeedae88a8b8da1054c18c75b0e28a6973f16e
Ezel3935 <ezel.acer88@gmail.com>
2022-09-17 12:21:11 +0300

    ödev 1

---
+++ ezelInanJavaCamp2022/src/ezelInanJavaCamp2022/loopDemo.java
@@ -0,0 +1,46 @@
+package ezelInanJavaCamp2022;
+
+public class loopDemo {
+//bir değişken default olarak tanımlandıysa sadece tanımlandığı yerde geçerlidir. yani for döngüsünde tanımlandıysa for döngüsünde geçerli olacaktır.
+	// eğer int=1 yaparsak tanımlanan değişken hep 1 olarak kalacağı için sonsuz
+	// döngü(infinite loop) olacaktır.
+	public static void main(String[] args) {
+		// FOR
+		for (int i = 1; i < 10; i++) {
+			System.out.println(i);
+		} // döngü başladıktan sonra gerekli şartlar sağlanana kadar devam eder. yani bu
+			// durumda i 1 den başladığı için
+			// i=10 olana kadar kod çalışacaktır
+		System.out.println("Döngü sonlandı.");// i<10 ifadesine göre sonlandırma noktası oluşturuyoruz.
+
+		for (int i = 1; i < 10; i += 2) {// i+=2 yaptığımız için 2 li artacaktır sayı
+			System.out.println(i);
+		}
+		System.out.println("Döngü sonlandı.");// i<10 ifadesine göre sonlandırma noktası oluşturuyoruz.
+
+		
+		// WHILE
+		int i = 1;// değişken değerini burada belirtiyoruz
+		while (i < 10) {
+			System.out.println(i);
+			i++;
+		} // eğer int=1 yaparsak ve herhangi bir sonlandırma noktası belirtmezsek,
+			// tanımlanan değişken hep 1 olarak kalacağı için sonsuz döngü(infinite loop)
+			// olacaktır.bu yüzden default değişken tanımlanıyor.
+
+		System.out.println("while döngüsü çalıştırıldı.");
+		
+		
+		//DO- WHILE DÖNGÜSÜ
+		//do-while döngüsünde program satır satır çalışır.yani j=100 yaparsak 100 yazdırır j<10 olsa bile ama
+		//wile döngüsünde şart sağlanıyorsa çalışır yani int=20 yaparsak yukarıdaki örnekte bu döngü de i<10 olduğu için while şartı sağlanamaz, döngüye girmez ve çalışmaz 
+		int j=1;
+		do {
+			System.out.println(j);
+			j++;
+		} while (j<10);
+		System.out.println("do-while döngüsü çalıştırıldı.");
+		
+	}
+
+}
---
+++ ezelInanJavaCamp2022/src/ezelInanJavaCamp2022/switchBlokları.java
@@ -0,0 +1,30 @@
+package ezelInanJavaCamp2022;
+
+public class switchBlokları {
+
+	public static void main(String[] args) {
+		//switch bloklarıyla dallandırma yapılır
+		char grade = 'C'; //verilen not ne ise aşağıdaki bloklardan geçerli olanı kullanır.
+		
+				switch (grade) {
+				case 'A':
+					System.out.println("Mükemmel, geçtiniz");
+					break;
+				case 'B':
+					System.out.println("çok güzel,geçtiniz");
+					break;
+				case 'C':
+					System.out.println("İyi,Geçtiniz");
+					break;
+				case 'D':
+					System.out.println("Geçtiniz");
+					break;
+				case 'E':
+					System.out.println("Kaldın,git çalış");
+					break;
+					default: System.out.println("Geçersiz not");
+					break;
+				}
+		}
+
+}
