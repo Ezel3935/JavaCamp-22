ac697b92d528a7cf7e92c667cb6efa1cc9cd74e6
ezel inan <ezel.acer88@gmail.com>
2022-09-20 21:58:02 +0300

    dnm2

---
+++ ezelInanJavaCamp2022/src/ezelInanJavaCamp2022/arkadasSayilar.java
@@ -0,0 +1,35 @@
+package ezelInanJavaCamp2022;
+
+public class arkadasSayilar {
+
+	public static void main(String[] args) {
+		// 220-284
+		int x = 220;
+		int y = 284;
+		int toplamx = 0;
+		int toplamy = 0;
+
+		for (int i = 1; i < x; i++) {
+			if (x % i == 0) {
+				toplamx = toplamx + i;
+			}
+
+		}
+		for (int i = 1; i < y; i++) {
+			if (y % i == 0) {
+				toplamy = toplamy + i;
+				
+			}
+			
+		}
+		if (x==toplamy && y==toplamx) {
+			System.out.println(" Arkadaş Sayıdır");
+			
+		} else {
+			System.out.println(" Arkadaş Sayı değildir");
+			
+			
+		}
+	}
+
+}
---
+++ ezelInanJavaCamp2022/src/ezelInanJavaCamp2022/sayiBulma.java
@@ -0,0 +1,23 @@
+package ezelInanJavaCamp2022;
+
+public class sayiBulma {
+
+	public static void main(String[] args) {
+		int[] numbers = new int[] { 1, 2, 5, 7, 9, 0 };
+		int findnumber = 5;
+		boolean exists=false;
+		
+		for (int number : numbers) {
+			if (number == findnumber) {
+				exists = true;
+				break;
+			}
+			
+		}
+		if (exists) {
+			System.out.println(findnumber + " mevcuttur");
+		}else {
+			System.out.println(findnumber + " mevcut değildir");
+		}
+	}
+}
